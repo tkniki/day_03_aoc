c54d28ac6dbb962d1fd1402d4b0b87c755c354be
tkniki <trankimniki@gmail.com>
2021-12-03 22:39:00 +0100

    inputs and arrays

--- day_03/Program.cs
+++ day_03/Program.cs
@@ -5,6 +5,7 @@ namespace day_03
 {
     class Program
     {
+        //part 1:
         static char FindMostCommonBit(string[] arr, int index)
         {
             char gammaBit;
@@ -52,10 +53,49 @@ namespace day_03
             return gammaRate;
         }
 
+        //part: 2
+
+        static string[] FindMostCommonBits (string[] arr)
+        {
+            int binaryLength = arr[0].Length;
+            string[] mostCommonBits = new string[binaryLength];
+            for (int i = 0; i < binaryLength; i++)
+            {
+                mostCommonBits[i] = FindMostCommonBit(arr, i).ToString();
+            }
+
+            return mostCommonBits;
+        }
+
+        /*static void O2GenAndCO2Gen (string[] arr)
+        {
+            int binaryLength = arr[0].Length;
+            char[] mostCommonBit = new char[binaryLength];
+            for (int i = 0; i < binaryLength; i++)
+            {
+                mostCommonBit[binaryLength] = FindMostCommonBit(arr, i);
+
+            }
+
+
+            for (int i = 0; i < mostCommonBit.Length; i++)
+            {
+                Console.WriteLine(mostCommonBit[i]);
+            }
+           
+        }*/
+        /*static string O2GenAndCO2Gen(string[] arr, char bit)
+        {
+            for (int i = 0; i < arr[0].Length; i++)
+            {
+
+            }
+        }*/
+
         static void Main(string[] args)
         {
-            string[] DiagnosticReport = new string[1000];
-            StreamReader file = new StreamReader("input.txt");
+            string[] DiagnosticReport = new string[12];
+            StreamReader file = new StreamReader("example.txt");
 
             for (int i = 0; i < DiagnosticReport.Length; i++)
             {
@@ -96,10 +136,15 @@ namespace day_03
                 Console.WriteLine(DiagnosticReport[i]);
             }*/
 
-            Console.WriteLine($"binary gamma: {binaryGammaRate}, binary epsilon: {binaryEpsilonRate}");
+            /*Console.WriteLine($"binary gamma: {binaryGammaRate}, binary epsilon: {binaryEpsilonRate}");
             Console.WriteLine($"decimal gamma: {gammaRate}, decimal epsilon: {epsilonRate}");
 
-            Console.WriteLine(gammaRate * epsilonRate);
+            Console.WriteLine(gammaRate * epsilonRate);*/
+
+            for (int i = 0; i < DiagnosticReport[0].Length; i++)
+            {
+                Console.WriteLine(FindMostCommonBits(DiagnosticReport)[i]);
+            }
         }
     }
 }
